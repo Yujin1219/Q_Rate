de09f45e073de7feb9910ea2eaba7266162e0762
Yujin1219 <jin-1219@naver.com>
2025-12-01 20:56:24 +0900

    [#43] Refactor: 섹션 순서 변경

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -66,10 +66,11 @@ export default function HomePage() {
     <div className="min-h-screen bg-gradient-to-br from-violet-50 via-purple-50 to-fuchsia-50">
       <Header />
       <HeroSection />
+      <FeaturesSection />
       <TemplateSection />
       <SurveyTypesSection />
       <ResultVisualizationSection />
-      <FeaturesSection />
+      
       <RecentSurveys surveys={surveys} />
       <FooterSection />
     </div>
