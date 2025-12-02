544c6381f60bc9de38286ba587eb086e3f714071
Yujin1219 <jin-1219@naver.com>
2025-12-02 10:47:50 +0900

    [#37] Fix: 페이지 라우팅 시 스크롤 위치 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,6 @@ import { useEffect } from 'react'
 
 function App() {
   useEffect(() => {
-    // 페이지 로드 시 스크롤 최상단으로 이동
-    window.scrollTo(0, 0);
-    
     // 브라우저 스크롤 복원 비활성화
     if ('scrollRestoration' in window.history) {
       window.history.scrollRestoration = 'manual';
